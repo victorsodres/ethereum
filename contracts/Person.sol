07b0fe68d4976f97339b75460532ea35bbf91b62
Victor Sodré <victor.sodres@gmail.com>
2017-11-25 17:02:38 -0200

    Criado contrato owner e usando no contrato Pessoa

--- contracts/Person.sol
+++ contracts/Person.sol
@@ -1,8 +1,10 @@
 pragma solidity ^0.4.18;
 
-contract Person {
-    // Atributos
-    address public accountOwner;
+contract Owner {
+    address public owner;
+}
+
+contract Person is Owner {
     string public nome;
     string public cpf;
     string public dataNascimento;
@@ -13,14 +15,14 @@ contract Person {
         cpf = _cpf;
         dataNascimento = _dataNascimento;
         email = _email;
-        accountOwner = msg.sender;
+        owner = msg.sender;
     }
 
     function mudarEmail(string _novoEmail) public returns (bool) {
         //verificar se a pessoa que esta executando é o dono do contrato
-        if (msg.sender == accountOwner) {
+        if (msg.sender == owner) {
             email = _novoEmail;
-            LogPersonEmailAlterado(accountOwner, _novoEmail);
+            LogPersonEmailAlterado(owner, _novoEmail);
         } else {
             revert();
         }
